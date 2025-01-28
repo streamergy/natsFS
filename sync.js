dcc78350e65b875b20d8990c2ff04641e175cdab
Daniel Dakhno <dakhnod@gmail.com>
2025-01-28 17:47:01 +0100

    cleanup

--- sync.js
+++ sync.js
@@ -14,11 +14,9 @@ parser.add_argument('-b', '--bucket', { help: 'NATS object bucket name' })
 parser.add_argument('-m', '--mount', { help: 'Folder to sync objects to' })
 parser.add_argument('-c', '--config', { help: 'Path to config file. CLI options override options from the file' })
 parser.add_argument('-1', '--once', { help: 'Only run sync once, don\'t listen for NATS updates' })
-parser.add_argument('-to', '--timeout', { help: 'Timeout for readStream after x ms (default: 10000 = 10s)'})
+parser.add_argument('--timeout', { help: 'Timeout for readStream after x ms (default: 10000 = 10s)', type: 'int', defaultValue: 10000 })
 
 let args = parser.parse_args();
-
-const timeout = args.timeout ?? 10000;
 
 if(args.config){
     const config = JSON.parse(fs.readFileSync(args.config));
@@ -51,10 +49,7 @@ async function pipeStream(readStream, writableStream) {
     const reader = readStream.getReader();
 
     return await new Promise((resolve, reject) => {
-        setTimeout(()=> {
-            reject();
-            return;
-        }, timeout)
+        setTimeout(reject, args.timeout);
 
         let totalSize = 0;
         reader.read().then(function processText({ done, value }) {
